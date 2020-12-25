709cc94b920f85e6041dd863abf9cd08218488e2
rezaa98 <45605134+rezaa98@users.noreply.github.com>
2020-12-25 19:42:29 +0700

    Teesting awal
    config ms azure

---
+++ resources/views/themes/ezone/partials/footer.blade.php
@@ -0,0 +1,71 @@
+<!-- footer area start -->
+<footer class="footer-area">
+    <div class="footer-top-area pt-70 pb-35 wrapper-padding-5 gray-bg">
+        <div class="container-fluid">
+            <div class="widget-wrapper">
+                <div class="footer-widget mb-30">
+                    <a href="#"><img src="{{ asset('themes/ezone/assets/img/logo/2.png') }}" alt=""></a>
+                    <div class="footer-about-2">
+                        <p>Chesspedia is a chess brand from Malang, Indonesia</p>
+                    </div>
+                </div>
+                <div class="footer-widget mb-30">
+                    <h3 class="footer-widget-title-5">Contact Info</h3>
+                    <div class="footer-info-wrapper-3">
+                        <div class="footer-address-furniture">
+                            <div class="footer-info-icon3">
+                                <span>Address: </span>
+                            </div>
+                            <div class="footer-info-content3">
+                                <p>Brawijaya University, Malang, id</p>
+                            </div>
+                        </div>
+                        <div class="footer-address-furniture">
+                            <div class="footer-info-icon3">
+                                <span>Phone: </span>
+                            </div>
+                            <div class="footer-info-content3">
+                                <p>+62 81414343494</p>
+                            </div>
+                        </div>
+                        <div class="footer-address-furniture">
+                            <div class="footer-info-icon3">
+                                <span>E-mail: </span>
+                            </div>
+                            <div class="footer-info-content3">
+                                <p><a href="#"> admin@chesspedia.id</a> <br><a href="#"> admin@chesspedia.id</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-widget mb-30">
+                    <h3 class="footer-widget-title-5">Newsletter</h3>
+                    <div class="footer-newsletter-2">
+                        <p>Send us your mail or next updates</p>
+                        <div id="mc_embed_signup" class="subscribe-form-5">
+                            <form action="http://devitems.us11.list-manage.com/subscribe/post?u=6bbb9b6f5827bd842d9640c82&amp;id=05d85f18ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll" class="mc-form">
+                                    <input type="email" value="" name="EMAIL" class="email" placeholder="Enter mail address" required>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div class="mc-news" aria-hidden="true"><input type="text" name="b_6bbb9b6f5827bd842d9640c82_05d85f18ef" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom ptb-20 gray-bg-8">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <div class="copyright-furniture">
+                        <p>Copyright © <a href="https://chesspedia.my.id/">Chesspedia</a> 2020 . All Right Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
